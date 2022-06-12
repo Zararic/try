f3cb586cd61853e972c31634176451af3c0d9306
Zararic <riccardobondisi@gmail.com>
2022-06-12 20:16:15 +0200

    add

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to("h1", { autoAlpha: 1, duration: 3.5 });
 
-tl.to(".container", { y: "110vh", opacity: 1, duration: 2.5 }, "-=3");
+tl.to(".container", { y: "110vh", opacity: 1, duration: 2 }, "-=3");
